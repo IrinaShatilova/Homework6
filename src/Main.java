b67ee93ed4ba0e34d566a2577c2e035c9c223dc2
shati <shatilova_ira@mail.ru>
2022-11-21 22:43:27 +0600

    1 Task (4 задачи блока 1)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Homerwork6");
+    public static void main(String[] args) { task1(); task2(); task3(); task4(); }
+        public static void task1() {
+            System.out.println("Homework5");
+            //Задание 1.1
+            System.out.println(" ");
+            System.out.println("Задание 1.1");
+            for(int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+        }
+
+        public static void task2() {
+            //Задание 1.2
+            System.out.println(" ");
+            System.out.println("Задание 1.2");
+            for(int i = 10; i >= 1; i--) {
+                System.out.println(i);
+            }
+        }
+        public static void task3() {
+            //Задание 1.3
+            System.out.println(" ");
+            System.out.println("Задание 1.3");
+            for(int i = 0; i <= 17; i += 2) {
+                System.out.println(i);
+            }
+        }
+    public static void task4() {
+        //Задание 1.4
+        System.out.println(" ");
+        System.out.println("Задание 1.4");
+        for(int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
     }
-}
+   }
